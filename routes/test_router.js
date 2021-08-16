89fa312465ec25708c9a3c93bbf95e3b71763b74
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-08-16 19:06:35 +1000

    Deleted other folders, check dom branch to view original files
    The repository structure may changed due to react app creation. Thus, clone the main branch and renamed with dom. Please view dom to view the previous DOM files.

--- routes/test_router.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const test_router = express.Router()
-const test_controller = require('../controller/test_controller')
-
-test_router.get('/', test_controller.send_test_message)
-
-module.exports = test_router
