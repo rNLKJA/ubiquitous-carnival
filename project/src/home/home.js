0f6965d3f4e205c4e0bc0652aa548857050f4143
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-04 14:58:22 +1000

    updated home page

--- project/src/home/home.js
+++ project/src/home/home.js
@@ -1,19 +1,11 @@
 // import required dependencies
 import React from "react";
+import img from "./home-gif.gif";
 
 const Home = () => {
   return (
     <React.Fragment>
-      <iframe
-        title="home-gif"
-        src="https://giphy.com/embed/RN89lrpGlBlBbspcef"
-        width="100%"
-        height="100%"
-        // style={{ position: "absolute" }}
-        frameBorder="0"
-        class="giphy-embed"
-        allowFullScreen
-      ></iframe>
+      <img title="home-gif" src={img} width="50%" alt="" />
     </React.Fragment>
   );
 };
