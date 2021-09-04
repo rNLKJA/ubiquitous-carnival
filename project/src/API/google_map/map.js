864edd4a6b5ed436683e009f4d2e4aad9a60e102
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-04 14:52:32 +1000

    staged google map api, now the map api should display the marker dynamically based on user input

--- project/src/API/google_map/map.js
+++ project/src/API/google_map/map.js
@@ -1,11 +1,115 @@
 // import required dependencies
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React from "react";
+import { useState } from "react";
+import GoogleMapReact from "google-map-react";
+import marker from "./google-maps.png";
+// import { Slider } from "@material-ui/core";
 
+// set marker
+const Marker = ({ text }) => {
+  return (
+    <div className="marker">
+      <div>
+        {/* this marker image was downloaded from the internet */}
+        <img src={marker} alt="marker" className="marker" />
+        {/* set displayed text */}
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+// create google map api
 const Map = () => {
-	return <h2>
-		This is a map
-	</h2>
-}
+  // set default geometry information
+  const [coords, setCoords] = useState({
+    lat: -37.7982,
+    lng: 144.961,
+    desp: "The University of Melbourne",
+  });
 
-export default Map
+  // default display information
+  const defaultProps = {
+    center: {
+      lat: coords.lat,
+      lng: coords.lng,
+    },
+    zoom: 14,
+  };
+
+  // handle change geolocation info
+  const handleChange = (e) => {
+    // obtain the targe name and its value
+    const name = e.target.name;
+    const value = e.target.value;
+
+    // change the geolocation valeu
+    setCoords({ ...coords, [name]: value });
+  };
+
+  // handle geolocation update function
+  const handleSubmit = (event) => {
+    // prevent web page auto refresh
+    event.preventDefault();
+  };
+
+  // render the map API
+  return (
+    // Important! Always set the container height explicitly
+    <div className="map-container">
+      <div style={{ height: "80vh", width: "100%" }} className="google-map">
+        {/* google map api connection setup */}
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyBGlgQ8EI9WBIpx9HOraQyJAD7fFLbrWvE" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+          <Marker lat={coords.lat} lng={coords.lng} text={coords.desp} />
+        </GoogleMapReact>
+      </div>
+      <form className="form">
+        <div className="form-control">
+          <label htmlFor="latitude">Latitude: </label>
+          <input
+            id="latitude"
+            type="number"
+            name="lat"
+            value={coords.lat}
+            step="0.0001"
+            onChange={handleChange}
+          ></input>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="longitude">Longitude: </label>
+          <input
+            id="longitude"
+            type="number"
+            name="lng"
+            value={coords.lng}
+            step="0.0001"
+            onChange={handleChange}
+          ></input>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="description">Description: </label>
+          <input
+            id="description"
+            type="text"
+            name="desp"
+            value={coords.text}
+            onChange={handleChange}
+          ></input>
+        </div>
+
+        {/* this submit buttom will update the geolocation */}
+        <button type="submit" className="btn" onClick={handleSubmit}>
+          Change Location
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Map;
