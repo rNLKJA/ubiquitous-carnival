bd90fdd5928b73c6e0d72a8155baed0ded2dc3f8
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-04 17:27:56 +1000

    updated calendar function, now the web should display a simple calendar

--- project/src/API/google_calendar/Calendar.js
+++ project/src/API/google_calendar/Calendar.js
@@ -1,13 +1,26 @@
 // import required dependencies
 import React from "react";
 import { useState, useEffect } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
-const Calendar = () => {
+const ReactCalendar = () => {
+  const [date, setDate] = useState(new Date());
+
+  // create onchange function
+  const ChangeHandler = () => {
+    setDate(date);
+  };
+
+  // return calendar component
   return (
     <React.Fragment>
       <h2>This is a Calendar</h2>
+      <div className="calendar">
+        <Calendar onChange={ChangeHandler} value={date}></Calendar>
+      </div>
     </React.Fragment>
   );
 };
 
-export default Calendar;
+export default ReactCalendar;
