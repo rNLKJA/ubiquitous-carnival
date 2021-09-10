250a1a840963f005badfcca3362d37560d0e2f9b
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-10 17:02:17 +1000

    Created contact display blocks

--- src/API/contact/Contact.js
+++ src/API/contact/Contact.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./contact.css";
+import { useContacts } from "../../BackEndAPI/contactAPI";
 
 const Contact = () => {
+  const contacts = useContacts();
   return (
     <div className="sub-container">
-      <h1>Contact lists</h1>
+      <div className="contact">
+        <div className="contactList">
+          <div className="contactList-items">{contacts[0]}</div>
+        </div>
+        <div className="contactDetail"></div>
+      </div>
     </div>
   );
 };
