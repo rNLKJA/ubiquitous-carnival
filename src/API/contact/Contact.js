8e213f83bed5a4eb7301c6f71d8f984401e0452e
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-10 17:16:56 +1000

    Staged contact code

--- src/API/contact/Contact.js
+++ src/API/contact/Contact.js
@@ -4,11 +4,12 @@ import { useContacts } from "../../BackEndAPI/contactAPI";
 
 const Contact = () => {
   const contacts = useContacts();
+  console.log(contacts);
   return (
     <div className="sub-container">
       <div className="contact">
         <div className="contactList">
-          <div className="contactList-items">{contacts[0]}</div>
+          <div className="contactList-items"></div>
         </div>
         <div className="contactDetail"></div>
       </div>
--- src/BackEndAPI/contactAPI.js
+++ src/BackEndAPI/contactAPI.js
@@ -1,100 +1,97 @@
 import { useState, useEffect } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 const BASE_URL = "http://localhost:5000";
 
 axios.defaults.withCredentials = true;
 
 function getContacts() {
-    const endpoind = BASE_URL+'/contact/showContact'
-    return axios.get(endpoind).then(res => res.data)
+  const endpoind = BASE_URL + "/contact/showContact";
+  return axios.get(endpoind).then((res) => res.data);
 }
 
-// json file of contact info 
+// json file of contact info
 function createContact(contactInfo) {
-    const endpoind = BASE_URL+'/contact/createContact'
-    return axios.post(endpoind, contactInfo).then(res => res.data)
+  const endpoind = BASE_URL + "/contact/createContact";
+  return axios.post(endpoind, contactInfo).then((res) => res.data);
 }
 
 //object ID as input
 function ShowOneContact(contactObjectId) {
-    const endpoint = BASE_URL+'/contact/showOneContact'
-    return axios.post(endpoint, {contactObjectId: contactObjectId}).then(res => res.data)
+  const endpoint = BASE_URL + "/contact/showOneContact";
+  return axios
+    .post(endpoint, { contactObjectId: contactObjectId })
+    .then((res) => res.data);
 }
 
-export function useContacts(){
-    const [loading, setLoading] = useState(true);
-    const [contacts, setContacts] = useState([]);
-    const [error, setError] = useState(false);
+export function useContacts() {
+  const [loading, setLoading] = useState(true);
+  const [contacts, setContacts] = useState([]);
+  const [error, setError] = useState(false);
 
-
-    useEffect(() => {
-        getContacts()
-          .then(contacts => {
-            setContacts(contacts);
-            setLoading(false);
-          })
-          .catch(e => {
-            console.log(e);
-            setError(e);
-            setLoading(false);
-          });
-      }, []);
-      return {
-            loading,
-            contacts,
-            error
-      }
+  useEffect(() => {
+    getContacts()
+      .then((contacts) => {
+        setContacts(contacts);
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.log(e);
+        setError(e);
+        setLoading(false);
+      });
+  }, []);
+  return {
+    loading,
+    contacts,
+    error,
+  };
 }
 
-export function useShowOneContact(){
-    const [loading, setLoading] = useState(true);
-    const [contact, setContact] = useState();
-    const [error, setError] = useState(false);
+export function useShowOneContact() {
+  const [loading, setLoading] = useState(true);
+  const [contact, setContact] = useState();
+  const [error, setError] = useState(false);
 
-
-    useEffect(() => {
-        ShowOneContact()
-          .then(contact => {
-            setContact(contact);
-            setLoading(false);
-          })
-          .catch(e => {
-            console.log(e);
-            setError(e);
-            setLoading(false);
-          });
-      }, []);
-      return {
-            loading,
-            contact,
-            error
-      }
-
-
+  useEffect(() => {
+    ShowOneContact()
+      .then((contact) => {
+        setContact(contact);
+        setLoading(false);
+      })
+      .catch((e) => {
+        console.log(e);
+        setError(e);
+        setLoading(false);
+      });
+  }, []);
+  return {
+    loading,
+    contact,
+    error,
+  };
 }
 //TODO:Put that function in Pages instead of API
-export function useCreateContact(){
-    const [email, setEmail] = useState([]);
-    const [phone, setPhone] = useState([]);
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [occupation, setOccupation] = useState("");
-    const [portriat, setPortriat] = useState("");
-    const [note, setNote] = useState("");
+export function useCreateContact() {
+  const [email, setEmail] = useState([]);
+  const [phone, setPhone] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [occupation, setOccupation] = useState("");
+  const [portriat, setPortriat] = useState("");
+  const [note, setNote] = useState("");
 
-    function onSubmit(){
-        createContact({
-            email: email,
-            phone: phone,
-            firstName: firstName,
-            lastName: lastName,
-            occupation: occupation,
-            portriat: portriat,
-            note: note
-        })
-        //redirect if needed
-    }
-    return //html for form 
-
+  function onSubmit() {
+    createContact({
+      email: email,
+      phone: phone,
+      firstName: firstName,
+      lastName: lastName,
+      occupation: occupation,
+      portriat: portriat,
+      note: note,
+    });
+    //redirect if needed
+  }
+  return; //html for form
 }
