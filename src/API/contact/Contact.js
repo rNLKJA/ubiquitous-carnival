141527a05ea27cf9cf0d3ee7bf154e51ac099aba
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-10 18:11:42 +1000

    updated contact information

--- src/API/contact/Contact.js
+++ src/API/contact/Contact.js
@@ -3,13 +3,24 @@ import "./contact.css";
 import { useContacts } from "../../BackEndAPI/contactAPI";
 
 const Contact = () => {
-  const contacts = useContacts();
+  const { loading, contacts, error } = useContacts();
   console.log(contacts);
+
   return (
     <div className="sub-container">
       <div className="contact">
         <div className="contactList">
-          <div className="contactList-items"></div>
+          <div className="contactList-items">
+            {contacts.map((person) => (
+              <Person prop={person} key={person._id} />
+            ))}
+            {contacts.map((person) => (
+              <Person prop={person} key={person._id} />
+            ))}
+            {contacts.map((person) => (
+              <Person prop={person} key={person._id} />
+            ))}
+          </div>
         </div>
         <div className="contactDetail"></div>
       </div>
@@ -18,3 +29,17 @@ const Contact = () => {
 };
 
 export default Contact;
+
+export const Person = ({ prop }) => {
+  return (
+    <div className="person">
+      <p>First Name: {prop.contact.firstName}</p>
+      <p>Last Name: {prop.contact.lastName}</p>
+      <p>Occupation: {prop.contact.occupation}</p>
+    </div>
+  );
+};
+
+export const PersonInfo = ({ prop }) => {
+  return <div className="personInfo"></div>;
+};
