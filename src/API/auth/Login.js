5cb712ad84ccd0497e1f5dd77c572c4d53294282
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-13 10:33:15 +1000

    Updated login css

--- src/API/auth/Login.js
+++ src/API/auth/Login.js
@@ -1,75 +1,82 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
+// import "bootstrap/dist/css/bootstrap.min.css"
+import "./login.css";
 
-import "bootstrap/dist/css/bootstrap.min.css"
+import Axios from "axios";
 
-import Axios from 'axios'
-
-import Registration from './Registration'
+import Registration from "./Registration";
 
 Axios.defaults.withCredentials = true;
 
 const Login = () => {
-    
-    const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
-    const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("");
 
-    const [loginState, setLoginState] = useState({status : false , username : "not login yet"});
+  const [loginState, setLoginState] = useState({
+    status: false,
+    username: "not login yet",
+  });
 
-    const handleLogin = (event) => {
-        event.preventDefault()
-        console.log("trying to login..." + username);
-        Axios.post("http://localhost:5000/user/login", {
-            userName: username,
-            password: password,
-        }).then((response) => {
-            console.log(response)
-            if (!response.data.auth){
-                console.log("login fail")
-                setLoginState({status : false , username : "login fail"});
-            } else {
-                console.log(response.data)
-                setLoginState({status:true,username : username})    
-                window.location.href = "/";
-            }
-        })
-        
-    }
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log("trying to login..." + username);
+    Axios.post("http://localhost:5000/user/login", {
+      userName: username,
+      password: password,
+    }).then((response) => {
+      console.log(response);
+      if (!response.data.auth) {
+        console.log("login fail");
+        setLoginState({ status: false, username: "login fail" });
+      } else {
+        console.log(response.data);
+        setLoginState({ status: true, username: username });
+        window.location.href = "/";
+      }
+    });
+  };
 
-    const handleSignup = (event) => {
-        window.location.href = "/signup";
-    }
+  const handleSignup = (event) => {
+    window.location.href = "/signup";
+  };
 
-    return (
-        <div className="sub-container">
-            Login <br/><br/>
-            <div>
-                UserName <br/>
-                <input type="text"
-                 value = {username}
-                 onChange={e=> setUsername(e.target.value)}
-                 />
+  return (
+    <div className="sub-container">
+      <div className="login-container">
+        <h1>CRM Login</h1>
 
-            </div>
-            <div>
-                password <br/><input type="password"
-                 value = {password}
-                 onChange = {e=>setPassword(e.target.value)}
-                 />
-            </div><br/><br/>
+        <div>
+          <label>Username :</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password :</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
 
-            <input type="button"
-                value= "login"
-                onClick={handleLogin}
-            />
-            <br/><br/>
+        <input
+          className="login-btn"
+          type="button"
+          value="login"
+          onClick={handleLogin}
+        />
 
-            <button className="btn btn-primary" onClick = {handleSignup}>Create a new account</button>
-            
-        </div> 
-    )
-}
-
+        <button className="signup-btn" onClick={handleSignup}>
+          Create a new account
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
