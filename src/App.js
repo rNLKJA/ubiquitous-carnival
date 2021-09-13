1a18fde86e2c4deb89b8b72110b4dcace9b83b86
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-13 10:07:25 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ import Record from "./API/record/Record";
 import Search from "./API/search/Search";
 import Error from "./API/error/Error";
 import Login from "./API/auth/Login";
-import Registration from "./API/auth/Registration"
-import ProtectedRouters from "./API/auth/ProtectedRouter"
-import LogOut from "./API/auth/Logout"
+import Registration from "./API/auth/Registration";
+import ProtectedRouters from "./API/auth/ProtectedRouter";
+import LogOut from "./API/auth/Logout";
 
 // defined the map function
 function App() {
@@ -28,33 +28,42 @@ function App() {
         {/* <NavbarTop /> */}
         <Navbar />
         <Switch>
-          <ProtectedRouters exact path="/" component={Home}>
-          </ProtectedRouters>
-          <Router path="/login" component={Login}>
-          </Router>
-          <Router path="/logout" component = {LogOut}>
-          </Router>
-          <Router path="/signup" >
+          <ProtectedRouters exact path="/" component={Home}></ProtectedRouters>
+          <Router exact path="/login" component={Login}></Router>
+          <Router exact path="/logout" component={LogOut}></Router>
+          <Router exact path="/signup">
             <Registration />
           </Router>
 
-
-
-          <ProtectedRouters exact path="/contact" component = {Contact}>
-          </ProtectedRouters>
-          <Route exact path="/map" component = {Map}>
-          </Route>
+          <ProtectedRouters
+            exact
+            path="/contact"
+            component={Contact}
+          ></ProtectedRouters>
+          <ProtectedRouters
+            exact
+            path="/map"
+            component={Map}
+          ></ProtectedRouters>
           {/* <Route path="/calendar">
             <Calendar />
           </Route> */}
-          <ProtectedRouters exact path="/record" component = {Record}>
-          </ProtectedRouters>
-          <ProtectedRouters exact path="/search" component = {Search}>
-          </ProtectedRouters>
-          <ProtectedRouters exact path="/setting" component = {Person}>
-          </ProtectedRouters>
+          <ProtectedRouters
+            exact
+            path="/record"
+            component={Record}
+          ></ProtectedRouters>
+          <ProtectedRouters
+            exact
+            path="/search"
+            component={Search}
+          ></ProtectedRouters>
+          <ProtectedRouters
+            exact
+            path="/setting"
+            component={Person}
+          ></ProtectedRouters>
 
-          
           <Route path="*">
             <Error msg={"AHHHHHHHH"} />
           </Route>
