43c844097851c94d4dda4d579102f3d5788593b5
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-13 11:50:05 +1000

    Updated contact css

--- src/API/contact/Contact.js
+++ src/API/contact/Contact.js
@@ -1,10 +1,26 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./contact.css";
 import { useContacts } from "../../BackEndAPI/contactAPI";
+// import { requirePropFactory } from "@material-ui/core";
+import Loading from "./pending";
 
 const Contact = () => {
   const { loading, contacts, error } = useContacts();
-  console.log(contacts);
+  const [oneContact, setOneContact] = useState({
+    firstName: "",
+    lastName: "",
+    email: [],
+    linkedAccount: null,
+    occupation: "",
+    phone: [],
+    selected: false,
+  });
+
+  useEffect(() => {
+    console.log("Display Contact Changed");
+    console.log(oneContact);
+  }, [oneContact]);
 
   return (
     <div className="sub-container">
@@ -12,36 +28,59 @@ const Contact = () => {
         <div className="contactList">
           <div className="contactList-items">
             {contacts.map((person) => (
-              <Person prop={person} key={person._id} />
-            ))}
-            {contacts.map((person) => (
-              <Person prop={person} key={person._id} />
-            ))}
-            {contacts.map((person) => (
-              <Person prop={person} key={person._id} />
-            ))}
-
-            {contacts.map((person) => (
-              <Person prop={person} key={person._id} />
-            ))}
-            {contacts.map((person) => (
-              <Person prop={person} key={person._id} />
-            ))}
-            {contacts.map((person) => (
-              <Person prop={person} key={person._id} />
-            ))}
-            {contacts.map((person) => (
-              <Person prop={person} key={person._id} />
-            ))}
-            {contacts.map((person) => (
-              <Person prop={person} key={person._id} />
-            ))}
-            {contacts.map((person) => (
-              <Person prop={person} key={person._id} />
+              <Person
+                prop={person}
+                key={person._id}
+                setOneContact={setOneContact}
+              />
             ))}
           </div>
         </div>
-        <div className="contactDetail"></div>
+        {!oneContact.selected && (
+          <div className="contactDetail">
+            <Loading msg={"Please Select A Contact"} />
+          </div>
+        )}
+        {oneContact.selected && (
+          <div className="contactDetail">
+            <img src={require("./portrarit.png")} alt="protrait.png" />
+            <div className="name">
+              <label>First Name: </label>
+              {oneContact.firstName}
+            </div>
+            <div className="name">
+              <label>Last Name: </label>
+              {oneContact.lastName}
+            </div>
+            <div className="occupation">
+              <label>Occupation: </label>
+              {oneContact.occupation}
+            </div>
+            <div className="linkedAccount">
+              <label>Linked Account: </label>
+              {oneContact.linkedAccount}
+            </div>
+            <div className="email">
+              {oneContact.email.map((mail) => {
+                return (
+                  <label key={new Date().toISOString()}>
+                    Email Address: {mail}
+                  </label>
+                );
+              })}
+            </div>
+            <div className="phone">
+              {oneContact.phone.map((phone) => {
+                return (
+                  <label key={new Date().toISOString()}>Phone: {phone}</label>
+                );
+              })}
+            </div>
+            <div className="note" style={{ height: "300px" }}>
+              <input value={oneContact.note}></input>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
@@ -49,9 +88,12 @@ const Contact = () => {
 
 export default Contact;
 
-export const Person = ({ prop }) => {
+export const Person = ({ prop, setOneContact }) => {
   return (
-    <div className="person">
+    <div
+      className="person"
+      onClick={() => setOneContact({ ...prop.contact, selected: true })}
+    >
       <p>First Name: {prop.contact.firstName}</p>
       <p>Last Name: {prop.contact.lastName}</p>
       <p>Occupation: {prop.contact.occupation}</p>
---
+++ src/API/contact/pending.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import { useState, useEffect } from "react";
+// import { Redirect } from "react-router-dom";
+import "./pending.css";
+
+const pending = ({ msg }) => {
+  return (
+    <div className="pending" style={{ height: "300px" }}>
+      <p>LOADING</p>
+      <p>(´ ｡ • ᵕ • ｡ `)</p>
+      <p>{msg}</p>
+    </div>
+  );
+};
+
+export default pending;
