9c4673501fd6b7b721516873a8b3d52f5be5060c
Harrison Huang <hohuang@student.unimelb.edu.au>
2021-09-27 05:50:15 +1000

    Update record Search Function

--- controller/recordController.js
+++ controller/recordController.js
@@ -17,14 +17,14 @@ const createRecord = async (req, res) => {
     /*
     request header: username
     request body:
-    {   
-        "contact_id" : 12545485,
-        "clientUsername" : "Harrison12138" or "null",
+    {  
+        "contact_id": 12354325
+        "meetingPerson": meetingPerson,
         "dateTime": "10/10/2000",
-        "location": "University of Melbourne"
+        "location": "University of Melbourne",
     }*/
     const username = req.header.userName
-    const {contact_id, clientUsername, dateTime, location} = req.body
+    const {contact_id, meetingPerson, dateTime, location, linkedAccount} = req.body
     if (dateTime==null) dateTime = new Date.now()
     try {
         meetingPerson = await Contact.findOne({_id: mongoose.Types.ObjectId(contact_id)}).lean(),
@@ -33,7 +33,7 @@ const createRecord = async (req, res) => {
             "dateTime": dateTime,
             "location": location,
             "ownerAccount" : username,
-            "linkedAccount" : clientUsername
+            "linkedAccount" : linkedAccount
         })
 
         newRecord.save()
@@ -58,6 +58,41 @@ const showAllRecords = async (req,res) => {
     const ownerAccount = await User.findOne({_id: mongoose.Types.ObjectId(req.user._id)}).populate("RecordList.record").lean()
     res.json(ownerAccount.recordList)
 }
+
+const searchRecord = async (req, res) => {
+    const validationErrors = expressValidator.validationResult(req)
+    if (!validationErrors.isEmpty()){
+        return res.status(422).render('error', {errorCode: '422', message: 'Search works on alphabet characters only.'})
+    }
+    var query = {}
+
+    if (req.body.meetingPerson != ''){
+        query["meetingPerson"] = {$regex: new RegExp(req.body.meetingPerson, 'i') }
+    }
+    if (req.body.location != ''){
+        query["location"] = {$regex: new RegExp(req.body.location, 'i') }
+    }
+    if (req.body.occupation != ''){
+        query["occupation"] = {$regex: new RegExp(req.body.occupation, 'i') }
+    }
+    if (req.body.dateTime != ''){
+        try {
+            const searchDate = new Date(req.body.dateTime)
+            var matchRecords = await Record.find({dateTime: {$lt: searchDate.getTime()}})
+            res.json(matchRecords)
+            return
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    try {
+		const records = await Record.find(query).lean()
+		res.json(records)	
+	} catch (err) {
+		console.log(err)
+	}
+}
+
 
 module.exports = {
     createRecord,
