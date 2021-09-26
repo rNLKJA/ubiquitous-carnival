f7b1891770f648a7e728eb5b85224eb38be9da8b
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-26 22:00:30 +1000

    Added mobile screen size css

--- src/API/contact/Contact.js
+++ src/API/contact/Contact.js
@@ -17,35 +17,69 @@ const Contact = () => {
     selected: false,
   });
 
+  const [filter, setFilter] = useState("");
+  var Contacts = contacts;
+
   useEffect(() => {
-    console.log("Display Contact Changed");
-    console.log(oneContact);
-  }, [oneContact]);
+    // console.log("Display Contact Changed");
+    // console.log(oneContact);
+
+    if (filter == "") {
+      Contacts = contacts;
+    } else {
+      Contacts.filter((contact) =>
+        (contact.firstName + " " + contact.lastName)
+          .toLowerCase()
+          .match(filter.toLowerCase()),
+      );
+    }
+  }, [filter, Contacts]);
+
+  const screenWidth = window.innerWidth;
+
+  const handleChange = (e) => {
+    e.preventDefault;
+    setFilter(e.target.value);
+    console.log(filter);
+  };
 
   return (
     <div className="sub-container">
       <div className="contact">
         {/* display contact as a list */}
-        <div className="contactList">
-          <div className="contactList-items">
-            {contacts.map((person) => (
-              <Person
-                prop={person}
-                key={person._id}
-                setOneContact={setOneContact}
-              />
-            ))}
-          </div>
-        </div>
 
-        {/* display a specific information */}
-        {/* {!oneContact.selected && (
+        {screenWidth <= 375 && !oneContact.selected && (
+          <React.Fragment>
+            <input
+              className="search-box"
+              value={filter}
+              onChange={(e) => handleChange(e)}
+            ></input>
+
+            <div className="contactList">
+              <div className="contactList-items">
+                {Contacts.map((person) => (
+                  <Person
+                    prop={person}
+                    key={person._id}
+                    setOneContact={setOneContact}
+                  />
+                ))}
+              </div>
+            </div>
+          </React.Fragment>
+        )}
+
+        {screenWidth <= 375 && oneContact.selected && (
           <div className="contactDetail">
-            <Loading msg={"Please Select A Contact"} />
-          </div>
-        )} */}
-        {/* {oneContact.selected && (
-          <div className="contactDetail">
+            <button
+              className="back"
+              onClick={() => {
+                setOneContact({ ...oneContact, selected: false });
+              }}
+            >
+              Back
+            </button>
             <img
               src={require("./portrarit.png")}
               alt="protrait.png"
@@ -92,7 +126,81 @@ const Contact = () => {
               <input value={oneContact.note}></input>
             </div>
           </div>
-        )} */}
+        )}
+
+        {screenWidth > 375 && (
+          <div className="contactList">
+            <div className="contactList-items">
+              {contacts.map((person) => (
+                <Person
+                  prop={person}
+                  key={person._id}
+                  setOneContact={setOneContact}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {/* display a specific information if screen width greater than 375 px */}
+        {screenWidth > 375 && (
+          <div>
+            {!oneContact.selected && (
+              <div className="contactDetail">
+                <Loading msg={"Please Select A Contact"} />
+              </div>
+            )}
+            {oneContact.selected && (
+              <div className="contactDetail">
+                <img
+                  src={require("./portrarit.png")}
+                  alt="protrait.png"
+                  style={{ paddingTop: "15px" }}
+                />
+                <div className="name">
+                  <label>First Name: </label>
+                  {oneContact.firstName}
+                </div>
+                <div className="name">
+                  <label>Last Name: </label>
+                  {oneContact.lastName}
+                </div>
+                <div className="occupation">
+                  <label>Occupation: </label>
+                  {oneContact.occupation}
+                </div>
+                <div className="linkedAccount">
+                  <label>Linked Account: </label>
+                  {oneContact.linkedAccount}
+                </div>
+                <div className="email">
+                  {oneContact.email.map((mail) => {
+                    return (
+                      <label key={new Date().toISOString()}>
+                        Email Address: {mail}
+                      </label>
+                    );
+                  })}
+                </div>
+                <div className="phone">
+                  {oneContact.phone.map((phone) => {
+                    return (
+                      <label
+                        key={new Date().toISOString()}
+                        style={{ color: "rgb(47,71,137)" }}
+                      >
+                        Phone: {phone}
+                      </label>
+                    );
+                  })}
+                </div>
+                <div className="note" style={{ height: "300px" }}>
+                  <input value={oneContact.note}></input>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
@@ -104,7 +212,9 @@ export const Person = ({ prop, setOneContact }) => {
   return (
     <div
       className="person"
-      onClick={() => setOneContact({ ...prop.contact, selected: true })}
+      onClick={() => {
+        setOneContact({ ...prop.contact, selected: true });
+      }}
     >
       <div className="profile">
         <img
