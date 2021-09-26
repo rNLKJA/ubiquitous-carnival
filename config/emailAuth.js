4e4eb6c55ab05d0b3d3d65088ae0fd78b71a8824
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-26 18:09:51 +1000

    Fixed: cannot read data from the formdata object

--- config/emailAuth.js
+++ config/emailAuth.js
@@ -1,87 +1,94 @@
-const mongoose = require('mongoose');
-const nodemailer = require('nodemailer')
-const smtpTransport = require('nodemailer-smtp-transport')
-const EmailAuth = mongoose.model('EmailAuth')
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+const smtpTransport = require("nodemailer-smtp-transport");
+const EmailAuth = mongoose.model("EmailAuth");
 
-
-const transport = nodemailer.createTransport(smtpTransport({
-    host: 'smtp.gmail.com',
+const transport = nodemailer.createTransport(
+  smtpTransport({
+    host: "smtp.gmail.com",
     port: 465,
     secure: true,
-    auth:{
-        user:'team4399Auth@gmail.com',
-        pass:'Team4399.com'
-    }
-}))
+    auth: {
+      user: "team4399Auth@gmail.com",
+      pass: "Team4399.com",
+    },
+  }),
+);
 
 transport.verify(function (error, success) {
-    if (error) {
+  if (error) {
     console.log(error);
-    } else {
+  } else {
     console.log("Server is ready to take our messages");
-    }
+  }
 });
 
-const autoCodeGenerator = () =>{
-    var code = ""
-    for (let i=0; i<6;i++){
-        code += parseInt(Math.random()*10)
-    }
-    return code
-}
+const autoCodeGenerator = () => {
+  var code = "";
+  for (let i = 0; i < 6; i++) {
+    code += parseInt(Math.random() * 10);
+  }
+  return code;
+};
 
 // eamil reg for check form of input eamil
-const emailReg =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+const emailReg =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const emailAuthSend = async (req, res) => {
-    const email = req.body.email
-    console.log(req.body.email)
-    const authCode = autoCodeGenerator()
-    if (1){
-        transport.sendMail({
-            from: 'team4399Auth@gmail',
-            to: email,
-            subject: 'vertify your email with code',
-            html:`
+  const email = req.fields.email; // read email from the request formdata field
+  // console.log(email);
+
+  const authCode = autoCodeGenerator();
+  if (1) {
+    transport.sendMail(
+      {
+        from: "team4399Auth@gmail",
+        to: email,
+        subject: "vertify your email with code",
+        html: `
             <p>Hello!</p>
             <p>you are registing account in 4399CRM</p>
             <p>your verify code is：<strong style="color: #ff4e2a;">${authCode}</strong></p>
-            <p>***this code will expire in 5 mins***</p>`
-        },
-        function(error, data) {
-            console.log(error)
-            // assert(error, 500, "fail to send vertify code")
-            transport.close()
-        })
-        try{       
-            await EmailAuth.deleteMany({email:email})
-            const AuthDocument = new EmailAuth({email:email, authCode: authCode})
-            await AuthDocument.save()
-            setTimeout(async ()=> {
-                await EmailAuth.deleteMany({email:email})
-            }, 1000*60*5)    
-            res.send("your email code has been send!")
-        }catch(err){
-            console.log(err)
-        }
-
-    }else {
-        console.log("not valid email!")
-        // assert(false, 422, "not valid email!")
+            <p>***this code will expire in 5 mins***</p>`,
+      },
+      function (error, data) {
+        console.log(error);
+        // assert(error, 500, "fail to send vertify code")
+        transport.close();
+      },
+    );
+    try {
+      await EmailAuth.deleteMany({ email: email });
+      const AuthDocument = new EmailAuth({ email: email, authCode: authCode });
+      await AuthDocument.save();
+      setTimeout(async () => {
+        await EmailAuth.deleteMany({ email: email });
+      }, 1000 * 60 * 5);
+      res.send("your email code has been send!");
+    } catch (err) {
+      console.log(err);
     }
-}
+  } else {
+    console.log("not valid email!");
+    // assert(false, 422, "not valid email!")
+  }
+};
 
 const emailCodeVerify = async (req, res, next) => {
-    const verify = EmailAuth.findOne({email:req.body.email, authCode: req.body.authCode})
-    if (verify == null){
-        assert(false, 422, "something wrong with auth code")
-        return
-    }
-    await EmailAuth.deleteMany({email:req.body.email})
-    delete req.body.authCode
-    next()
-}
+  const verify = EmailAuth.findOne({
+    email: req.body.email,
+    authCode: req.body.authCode,
+  });
+  if (verify == null) {
+    assert(false, 422, "something wrong with auth code");
+    return;
+  }
+  await EmailAuth.deleteMany({ email: req.body.email });
+  delete req.body.authCode;
+  next();
+};
 
-module.exports= {
-    emailAuthSend,
-    emailCodeVerify
-}
+module.exports = {
+  emailAuthSend,
+  emailCodeVerify,
+};
