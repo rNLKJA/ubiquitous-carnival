270c660cbc8a8e1cdd4a650a73ea493fcc632565
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-27 13:16:11 +1000

    Updated navigation bar css

--- src/API/contact/addOneContact.js
+++ src/API/contact/addOneContact.js
@@ -6,29 +6,9 @@ const AddUser = () => {
   return (
     <React.Fragment>
       <div className="sub-container">
-        <NavBar />
-        {/* <div className="add-contact-container">
-          <a href="./addUser/qr-code">
-            <div className="qr-code">
-              <img src={qr_code} alt="qr-code"></img>
-              <h1>Scan QR Code</h1>
-            </div>
-          </a>
-     
-		      <a href="./addUser/user-id">
-            <div className="user-id">
-              <img src={user} alt="manual input"></img>
-              <h1>Search By User Id</h1>
-            </div>
-          </a>
-
-          <a href="./addUser/manual-input">
-            <div className="manual">
-              <img src={hand_write} alt="manual input"></img>
-              <h1>Manual Input</h1>
-            </div>
-          </a>
-        </div> */}
+        <div className="add-contact-container">
+          <NavBar />
+        </div>
       </div>
     </React.Fragment>
   );
