5c679c50ffcf85e69596f3cb91c3be30bb9cdabe
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-09-27 13:27:45 +1000

    changed form display, added back to last page button

--- src/API/contact/manual-input.js
+++ src/API/contact/manual-input.js
@@ -2,42 +2,45 @@ import React, { useState } from "react";
 import "./contact.css";
 // import qr_code from "./qr-code.png";
 // import hand_write from "./notes.png";
+import "./manual-input.css";
 
 const AddUser = () => {
   return (
     <React.Fragment>
       <div className="sub-container">
+        <a href="javascript:history.go(-1)">Back</a>
+
         <form
           className="contact-form"
           method="POST"
           action="http://localhost:5000/contact/createContact"
         >
-          <label for="lastName">First Name: </label>
+          <label for="lastName">Last Name: </label>
           <input
             name="lastName"
             type="text"
             placeholder="Please enter the First Name"
           ></input>
 
-          <label for="lastName">First Name: </label>
+          <label for="firstName">First Name: </label>
           <input
-            name="lastName"
+            name="firstName"
             type="text"
-            placeholder="Please enter the First Name"
+            placeholder="Please enter the Last Name"
           ></input>
 
-          <label for="lastName">First Name: </label>
+          <label for="email">E-mail: </label>
           <input
-            name="lastName"
-            type="text"
-            placeholder="Please enter the First Name"
+            name="email"
+            type="email"
+            placeholder="Please enter the E-mail"
           ></input>
 
-          <label for="lastName">First Name: </label>
+          <label for="phone">Phone: </label>
           <input
-            name="lastName"
+            name="phone"
             type="text"
-            placeholder="Please enter the First Name"
+            placeholder="Please enter the phone number"
           ></input>
         </form>
       </div>
