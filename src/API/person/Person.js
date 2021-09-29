97b436141c4cd176093d78e9c73ceab108365972
Lydia2025 <ghostsu2025@gmail.com>
2021-09-30 00:47:58 +0800

    Try to upload

--- src/API/person/Person.js
+++ src/API/person/Person.js
@@ -4,14 +4,13 @@ import axios from "axios";
 import LogoutUser from "../../hooks/useLogout";
 import "./person.css"
 import { useShowProfile} from "../../BackEndAPI/profileAPI";
-import { useUpdateProfile} from "../../BackEndAPI/profileAPI";
+
 
 const BASE_URL = "https://crm4399.herokuapp.com"
 
 const Person = () => {
     const { loading, profile, error } = useShowProfile();
     const [userName, setUserName] = useState("");
-    const [firstName, setFirstName] = useUpdateProfile("");
     const [person, setPerson] = useState({
         firstName: "",
         lastName: "",
@@ -19,10 +18,40 @@ const Person = () => {
         occupation: "",
         phone: [],
     });
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [occupation, setOccupation] = useState("");
 
     const inputEl = useRef(null);
 
-    const onButtonClick = () => {
+    const Submit = (e) => {
+        e.preventDefault();
+
+        const profile = {
+            firstName,
+            lastName,
+            email,
+            phone,
+            occupation,
+        };
+        axios
+            .post(BASE_URL + "/profile/updateProfile", profile)
+            .then(() => console.log("upload new information"))
+            .catch((err) => {
+                console.error(err);
+            });
+        
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPhone("");
+        setOccupation("");
+
+    };
+
+    /*const onButtonClick = () => {
         var element = inputEl.current;
         var oldhtml = element.innerHTML;
         var newobj = document.createElement('input');
@@ -33,10 +62,14 @@ const Person = () => {
         }
         element.innerHTML = '';
         element.appendChild(newobj);
-        useUpdateProfile(setFirstName(newobj));
+        setFirstName(newobj);
+        console.log("uploading");
         newobj.setSelectionRange(0, oldhtml.length);
         newobj.focus();
-    };
+
+        ref={inputEl} onClick={onButtonClick}
+    };*/
+
 
 
 
@@ -44,43 +77,48 @@ const Person = () => {
     return (
         <div className="sub-container">
             <div>
-                <h1>Personal Information</h1>
+                <form className="contact-form" method="POST" onSubmit={Submit}>
+                    <h1>Personal Information</h1>
 
-                <div className="basicInformation">
-                    <h2>Basic Information</h2>
-                    <div>
-                        <label>First name: </label>
-
-
-                            <div ref={inputEl} onClick={onButtonClick}>{profile.firstName}</div>
-
-
-                    <br/>
+                    <div className="basicInformation">
+                        <h2>Basic Information</h2>
+                        <div>
+                            <label>First name: </label>
+                                <div>{profile.firstName}</div>
+                            <input
+                                name="firstName"
+                                type="text"
+                                placeholder="Please enter the Last Name"
+                                onChange={(e) => setFirstName(e.target.value)}
+                                value={firstName}
+                                required
+                            ></input>
+                        </div>
+                        <div>
+                            <label>Given name: </label>
+                            {profile.lastName}
+                        </div>
+                        <div>
+                            <label>Occupation: </label>
+                            {profile.occupation}
+                        </div>
                     </div>
-                    <div>
-                        <label>Given name: </label>
-                        {profile.lastName}
-                    </div>
-                    <div>
-                        <label>Occupation: </label>
-                        {profile.occupation}
-                    </div>
-                </div>
 
 
-                <div className="contactInformation">
-                    <h2>Contact Information</h2>
-                    <div className="email">
-                        <label>email: </label>
-                        {profile.email}
+                    <div className="contactInformation">
+                        <h2>Contact Information</h2>
+                        <div className="email">
+                            <label>email: </label>
+                            {profile.email}
 
 
+                        </div>
+                        <div className="phone">
+                            <label>Phone: </label>
+                            {profile.phone}
+                        </div>
                     </div>
-                    <div className="phone">
-                        <label>Phone: </label>
-                        {profile.phone}
-                    </div>
-                </div>
+                </form>
             </div>
 
             <button className="btn btn-primary" onClick={LogoutUser}>logout</button>
