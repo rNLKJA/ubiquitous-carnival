aa1f1eb58084f7f5ec07cbf40342ebdc4251b674
Harrison Huang <hohuang@student.unimelb.edu.au>
2021-09-29 11:13:02 +1000

    Edit route of profile and record

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -8,17 +8,19 @@ const { upload } = require('../config/upload')
 require('../config/passport')(passport);
 // const photoUpload = require('../config/upload')
 
-profileRouter.post('/profile/updateProfile', passport.authenticate('jwt', { session: false }), (req,res) => profileController.updateProfile(req,res))
+profileRouter.post('/updateProfile', passport.authenticate('jwt', { session: false }), (req,res) => profileController.updateProfile(req,res))
 
-profileRouter.post('/profile/addPhone', passport.authenticate('jwt', { session: false }), (req,res) => profileController.addPhone(req,res))
+profileRouter.post('/addPhone', passport.authenticate('jwt', { session: false }), (req,res) => profileController.addPhone(req,res))
 
-profileRouter.post('/profile/delPhone', passport.authenticate('jwt', { session: false }), (req,res) => profileController.delPhone(req,res))
+profileRouter.post('/delPhone', passport.authenticate('jwt', { session: false }), (req,res) => profileController.delPhone(req,res))
 
-profileRouter.post('/profile/addEmail', passport.authenticate('jwt', { session: false }), (req,res) => profileController.addEmail(req,res))
+profileRouter.post('/addEmail', passport.authenticate('jwt', { session: false }), (req,res) => profileController.addEmail(req,res))
 
-profileRouter.post('/profile/delEmail', passport.authenticate('jwt', { session: false }), (req,res) => profileController.delEmail(req,res))
+profileRouter.post('/delEmail', passport.authenticate('jwt', { session: false }), (req,res) => profileController.delEmail(req,res))
 
-profileRouter.get('/profile/showProfile', passport.authenticate('jwt', { session: false }), (req,res) => profileController.showProfile(req,res))
+//profileRouter.get('/showProfile', passport.authenticate('jwt', { session: false }), (req,res) => profileController.showProfile(req,res))
+
+profileRouter.get('/showProfile', profileController.showProfile)
 
 profileRouter.post('/uploadUserImage', upload.single('portrait'), (req,res) => profileController.uploadPhoto(req,res))
 
