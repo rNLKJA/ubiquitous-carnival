9cb23dc7d7b0e239affcad38d6becf453d6ce9a1
Red_Peony <55085953+EcZww@users.noreply.github.com>
2021-09-29 16:07:21 +0800

    delete logout route

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,7 +6,6 @@ const userController = require('../controller/userController.js');
 const passport = require('passport');
 require('../config/passport')(passport);
 
-userRouter.get('/logout' , userController.handleLogout)
 userRouter.get('/jwtTest',passport.authenticate('jwt', { session: false }) , (req,res) => userController.isAuth(req,res))
 userRouter.post('/login',userController.handleLogin)
 userRouter.post('/signup', emailAuth.emailCodeVerify, userController.register)
