1254e3bbb1d990a3e3affdf741c18b143c1fb1af
Harrison Huang <hohuang@student.unimelb.edu.au>
2021-10-01 15:56:10 +1000

    Update jsdoc for record back-end api

--- src/BackEndAPI/recordAPI.js
+++ src/BackEndAPI/recordAPI.js
@@ -5,18 +5,30 @@ const BASE_URL = "http://localhost:5000";
 
 axios.defaults.withCredentials = true;
 
+/**
+* Back-End API: create a Record for the client
+* @param {json} recordInfo - the information of record being created
+* @return {express.Response} res - response from the back-end server.
+*/
 function createRecord(recordInfo) {
   const endpoint =  "/record/createRecord";
   return fetchClient.post(endpoint, recordInfo).then((res) => res.data);
 }
 
+/**
+* Back-End API: Show all the Record for the client
+* @return {express.Response} res - response from the back-end server.
+*/
 function showAllRecords() {
     const endpoint =  "/record/showRecord";
     return fetchClient.get(endpoint).then((res) => res.data
     );
 }
 
-
+/**
+* React_Use_Function: Show all the Record for the client
+* @return {json} json file contains the record information
+*/
 export function useShowAllRecords() {
     const [loading, setLoading] = useState(true);
     const [records, setRecords] = useState([]);
@@ -41,6 +53,9 @@ export function useShowAllRecords() {
     };
 }
 
+/**
+* React_Use_Function: Create the Record for the client
+*/
 export function useCreateRecord() {
     const [contact_id, setContactId] = useState("");
     const [clientUsername, setClientUsername] = useState("");
