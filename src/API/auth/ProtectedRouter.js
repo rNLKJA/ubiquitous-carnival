7ec23bed7b5c23926dd0ba4b13cae0c3b02f66db
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-10-04 11:05:31 +1100

    disabled loading status console.log

--- src/API/auth/ProtectedRouter.js
+++ src/API/auth/ProtectedRouter.js
@@ -1,24 +1,21 @@
-import React, { useContext } from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { UserContext } from '../../hooks/UserContext';
-import Loading from '../error/loading'; 
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { UserContext } from "../../hooks/UserContext";
+import Loading from "../error/loading";
 
+export default function ProtectedRouters(props) {
+  const { user, isLoading } = useContext(UserContext);
+  // console.log(user, isLoading);
 
-export default function ProtectedRouters(props) {   
-    const { user, isLoading } = useContext(UserContext); 
-    console.log(user, isLoading);
+  const { component: Component, ...rest } = props;
 
-    const { component: Component,
-        ...rest } = props;
+  if (isLoading) {
+    return <Loading />;
+  }
 
-      if(isLoading) {
-          return <Loading/>
-        }
-
-      if(user){
-        return ( <Route {...rest} render={(props) => (<Component {...props}/>)}/>)
-        } else {
-          return <Redirect to='/login'/> 
-        }
-
+  if (user) {
+    return <Route {...rest} render={(props) => <Component {...props} />} />;
+  } else {
+    return <Redirect to="/login" />;
+  }
 }
