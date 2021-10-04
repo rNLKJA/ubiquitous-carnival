92dad1a7b38a0862a2d382f8524f40013708d250
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-10-04 12:56:34 +1100

    disabled axios

--- src/hooks/useFindUser.js
+++ src/hooks/useFindUser.js
@@ -1,24 +1,26 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import fetchClient from '../API/axiosClient/axiosClient'
+import { useState, useEffect } from "react";
+// import axios from 'axios';
+import fetchClient from "../API/axiosClient/axiosClient";
 export default function useFindUser() {
-   const [user, setUser] = useState(null);
-   const [isLoading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [isLoading, setLoading] = useState(true);
 
-useEffect(() => {
-   async function findUser() {
-     await fetchClient.get('/user/jwtTest')  
-        .then(res => {
-        setUser(res.data);
-        setLoading(false);
-     }). catch(err => {
-        setLoading(false);
-    });
-  }
-   findUser();
-}, []);
-return {
-   user,
-   isLoading
-   }
+  useEffect(() => {
+    async function findUser() {
+      await fetchClient
+        .get("/user/jwtTest")
+        .then((res) => {
+          setUser(res.data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setLoading(false);
+        });
+    }
+    findUser();
+  }, []);
+  return {
+    user,
+    isLoading,
+  };
 }
