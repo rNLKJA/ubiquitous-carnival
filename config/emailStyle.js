3abd5b2a11a23713575a496b203f5c2dc3545de3
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-10-06 12:16:46 +1100

    Update emailStyle.js

--- config/emailStyle.js
+++ config/emailStyle.js
@@ -1,43 +1,23 @@
 const emailStyle = (authCode) => {
   return `
-	<div class='container' style='font-family: system-ui;
-																background: #383A59;
-																color:#f8f8f2;padding:0;
-																margin:0;
-																width:100%;
-																height:100%'>
-		<div class='heading' style="background-color: #282942;
-																width: 100%;
-																height: 80px;">
-			<h1 style="text-align: left;
-									padding: 20px;
-									color: #bd93f9;">
-									<span style="background: linear-gradient(120deg,#8be9fd,#ff79c6);
-															-webkit-background-clip: text;color: transparent;">
-															4399 CRM Authentication Code
-									</span>
-			</h1>
+	<div class='container' style='font-family: system-ui;background: #383A59;color:#f8f8f2;padding:0;margin:0;width:100%;height:100%'>
+		<div class='heading' style="background-color: #282942;width: 100%;height: 80px;">
+		<h1 style="text-align: left;padding: 20px;"><span style="background:linear-gradient(120deg,#8be9fd,#ff79c6);-webkit-background-clip: text;color: transparent;">4399 CRM Authentication Code</span></h1>
 		</div>
 
 		<div class='content' style="padding: 20px;">
-			<p>Hey friend, How you doing?</p>
+			<p style="color:white">Hey friend, How you doing?</p>
 
-			<p style='user-select:none'>
-				Here is your verification code，please enter your authentication code in <strong style="color:#ff5555;">5 minutes</strong>.
-			</p>
+			<p style='user-select:none;color:white'>Here is your verification code，please enter your authentication code in <strong style="color:#ff5555;">5 minutes</strong>.</p>
 
-			<h3 id='authCode' style='padding-left:20px;display:inline;color: 	#ff79c6;'>
-				${authCode}
-			</h3>
+			<h3 id='authCode' style='padding-left:20px;display:inline;color: 	#ff79c6;' >${authCode}</h3>
 
-			<p style='user-select:none'>
-				If you didn't request this code by setting up Verification, please go to your <a href="http://www.4399crm.com/" style="text-decoration: none;color: #6272a4;">Personal Information</a> page and change your password right away. 
-			</p>
+			<p style='user-select:none;color:white'>If you didn't request this code by setting up Verification, please go to your <a href="http://www.4399crm.com/" style="text-decoration: none;color: #6272a4;">Personal Information</a> page and change your password right away. </p>
 
 			<br />
 
-			<p>Hope you have an wonderful day!</p>
-			<p>Best Regards.</p>
+			<p style="color:white">Hope you have an wonderful day!</p>
+			<p style="color:white">Best Regards.</p>
 		</div>
 		
 		<div class='footer'>
