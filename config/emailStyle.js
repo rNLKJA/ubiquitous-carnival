d1bee0294eb0a53bdb3e554d4773a870a1b59a29
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-10-06 11:59:50 +1100

    Update emailStyle.js

--- config/emailStyle.js
+++ config/emailStyle.js
@@ -4,7 +4,23 @@ const emailStyle = (authCode) => {
 			<span style="background: linear-gradient(120deg,#8be9fd,#ff79c6);-webkit-background-clip: text;color: transparent;">4399 CRM Authentication Code</span>
 		</h1>
 	</div>
+
+	<div class='content'>
+		<p>Hey friend, How you doing?</p>
 		
+		<p style='user-select:none'>Here is your verification code，please enter your authentication code in <strong style="color:#ff5555;">5 minutes</strong>.</p>
+		
+		<h3 id='authCode' style='padding-left:20px;display:inline' onclick=copyContent() >$authCode</h3>
+		
+		<p style='user-select:none'>If you didn't request this code by setting up Verification, please go to your <a href="http://www.4399crm.com/">Personal Information</a> page and change your password right away. </p>
+		
+		<br />
+		
+		<p>Hope you have an wonderful day!</p>
+		<p>Best Regards.</p>
+	</div>
+		
+
 	<div class='footer'>
 		<hr style='color:green' />
 		<p style="text-align:right; padding:20px;color:#6272a4;">COMP30022 IT Project Team 4399</p>
