6736d8cd0609fdcbded7ad7eacba7a235833fc8c
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-10-07 20:12:54 +1100

    updated schema

--- models/recordSchema.js
+++ models/recordSchema.js
@@ -1,21 +1,23 @@
 // mongodb schema
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // create mongoose schema
 const recordSchema = new mongoose.Schema({
-    // Name of meeting person
-    meetingPerson: {type: mongoose.Schema.Types.ObjectId, ref: 'Contact'},
-	dateTime: {type: Date, required: true},
-    location: {type: String, required: true},
-    notes: {type: String},
-    pictures: [{
-		data: Buffer,
-		contentType: String 
-	}],
-    linkedAccount: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    ownerAccount: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
-})
+  // Name of meeting person
+  meetingPerson: { type: mongoose.Schema.Types.ObjectId, ref: "Contact" },
+  dateTime: { type: Date, required: true },
+  location: { type: String, required: true },
+  notes: { type: String },
+  pictures: [
+    {
+      data: Buffer,
+      contentType: String,
+    },
+  ],
+  linkedAccount: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  ownerAccount: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+});
 
-const Record = mongoose.model('Record', recordSchema)
+const Record = mongoose.model("Record", recordSchema);
 
-module.exports = Record
+module.exports = Record;
