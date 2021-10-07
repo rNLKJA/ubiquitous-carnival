77cbd50a00251c30886c0b6c47bbebfb02d2512f
Lydia2025 <ghostsu2025@gmail.com>
2021-10-08 02:01:15 +0800

    qrcode

--- src/API/contact/qr-code.js
+++ src/API/contact/qr-code.js
@@ -1,62 +1,98 @@
 
-import React, { useState, Component } from "react";
+import React, { useState, Component, useRef} from "react";
 import "./contact.css";
 import fetchClient from "../axiosClient/axiosClient";
 // import qr_code from "./qr-code.png";
 // import hand_write from "./notes.png";
-import QrReader from "react-qr-scanner";
+import QrReader from "react-qr-reader";
 
-class AddUser extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            result: 'Hold QR Code Steady and Clear to Scan',
+const AddUser = () => {
+    const qrRef = useRef(null);
+    //const [state,setState]=useState(false);
+    const [scanResultWebCam, setScanResultWebCam] = useState("");
+    const [userName, setUserName] = useState("");
+    const BASE_URL = "https://crm4399.herokuapp.com";
+    const submitUserID = (e) => {
+        e.preventDefault();
+
+        const contact = {
+            userName,
+        };
+
+        fetchClient
+            .post(BASE_URL + "/contact/createContact", contact)
+            .then(() => console.log("upload new information"))
+            .catch((err) => {
+                console.error(err);
+            });
+
+        setUserName("");
+    };
+
+
+    const [scanResultFile, setScanResultFile] =useState("");
+    const handleErrorFile = (error) =>{
+        console.log(error);
+    }
+    const handleScanFile = (result) => {
+        if (result){
+            setScanResultFile(result);
         }
-        this.handleScan = this.handleScan.bind(this)
     }
 
-    handleScan(result){
-        this.setState({
-            result: ''
-        })
+    const onScanFile = () => {
+        qrRef.current.openImageDialog();
+
     }
 
-    handleError(err){
-        console.error(err)
+    const handleErrorWebCam = (error) => {
+        console.log(error)
+
     }
-    render() {
-        const previewStyle={
-            height:700,
-            width:1000,
-            display: 'flex',
-            justifyContent:"center"
+
+    const handleScanWebCam = (result) => {
+        if (result){
+            setScanResultWebCam(result);
         }
-        const camStyle={
-            display: 'flex',
-            justifyContent: "center",
-            marginTop: '-50px'
-        }
-        const textStyle={
-            fontSize: '30px',
-            textAlign: 'center',
-            marginTop: '-50px'
-        }
-        return(
-            <React.Fragment>
-                <div style={camStyle}>
+
+    }
+
+
+    return (
+        <React.Fragment>
+            <div className="sub-container">
+                <h1 className="add-contact-container">QR Code</h1>
+                <div>
+{/*                    <button onClick={onScanFile}>Scan Qr Code</button>
                     <QrReader
-                        delay={100}
-                        style={previewStyle}
-                        onError={this.handleError}
-                        onScan={this.handleScan}
+                        ref={qrRef}
+                        delay = {300}
+                        style = {{width: '100%'}}
+                        onError={handleErrorFile}
+                        onScan={handleScanFile}
+                        legacyMode
                     />
+                    <div>Scanned Code:{scanResultFile}</div>*/}
+                    <h3>Qr Code Scan by Web Cam</h3>
+                    <QrReader
+                        delay = {300}
+                        style={{width: '100%'}}
+                        onError = {handleErrorWebCam}
+                        onScan = {handleScanWebCam}
+                    />
+                    <h3>UserID: {scanResultWebCam}</h3>
+                    <form className="newID" method="POST" onSubmit={submitUserID}>
+                        {setUserName.bind(this,scanResultWebCam)}
+                        <input type="submit" value="Confirm"/>
+                    </form>
+
                 </div>
-                <p style={textStyle}>
-                    {this.state.result}
-                </p>
-            </React.Fragment>
-        )
-    }
+
+
+            </div>
+        </React.Fragment>
+
+    )
 
 }
 
