1495623a680c1395b11478e304c5898dce82234a
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-10-08 15:36:56 +1100

    Added Heading and bottom navigation bar

--- src/API/contact/addOneContact.js
+++ src/API/contact/addOneContact.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import "./contact.css";
 import NavBar from "../nav/navbar-addContact";
+import NavBar_bottom from "../nav/Navbar";
+import Heading from "../heading/heading.jsx";
 
 const AddUser = () => {
   useEffect(() => {
@@ -8,11 +10,13 @@ const AddUser = () => {
   }, []);
   return (
     <React.Fragment>
+      <Heading />
       <div className="sub-container">
         <div className="add-contact-container">
           <NavBar />
         </div>
       </div>
+      <NavBar_bottom />
     </React.Fragment>
   );
 };
