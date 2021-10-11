aeffef121ed91afac5e09c6a56382ffd6644456e
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-10-11 17:47:46 +1100

    Convert form into a single component named display contact

--- src/API/contact/SelectedContact.jsx
+++ src/API/contact/SelectedContact.jsx
@@ -22,33 +22,8 @@ const SelectedContact = ({ setOneContact, oneContact, deleteHandler }) => {
         Back
       </button>
       {/* <img src={portrait} alt="protrait.png" style={{ paddingTop: "15px" }} /> */}
-      <form style={{ textAlign: "left", padding: "20px" }}>
-        <label>First Name: </label>
-        <input value={selectedContact.firstName} readOnly></input>
 
-        <label>Last Name: </label>
-        <input value={selectedContact.lastName} readOnly></input>
-
-        <label>Occupation: </label>
-        <input value={selectedContact.occupation} readOnly></input>
-
-        <label>Email Address</label>
-        {selectedContact.email.map((mail) => {
-          return <input value={mail} readOnly />;
-        })}
-
-        <label>Phone:</label>
-        {selectedContact.phone.map((phone) => {
-          return <input value={phone} readOnly />;
-        })}
-
-        <label>Notes:</label>
-        <textarea
-          style={{ height: "auto" }}
-          value={oneContact.note}
-          readOnly
-        ></textarea>
-      </form>
+      <DisplayContact contact={selectedContact} />
 
       <button
         className="delete-btn"
@@ -62,3 +37,40 @@ const SelectedContact = ({ setOneContact, oneContact, deleteHandler }) => {
 };
 
 export default SelectedContact;
+
+export const DisplayContact = ({ contact }) => {
+  if (contact.edit) {
+  } else {
+    return (
+      <form style={{ textAlign: "left", padding: "20px" }}>
+        <label>First Name: </label>
+        <input value={contact.firstName} readOnly></input>
+
+        <label>Last Name: </label>
+        <input value={contact.lastName} readOnly></input>
+
+        <label>Occupation: </label>
+        <input value={contact.occupation} readOnly></input>
+
+        <label>Email Address</label>
+        {contact.email.map((mail) => {
+          return <input value={mail} readOnly key={new Date().toISOString()} />;
+        })}
+
+        <label>Phone:</label>
+        {contact.phone.map((phone) => {
+          return (
+            <input value={phone} readOnly key={new Date().toISOString()} />
+          );
+        })}
+
+        <label>Notes:</label>
+        <textarea
+          style={{ height: "auto" }}
+          value={contact.note}
+          readOnly
+        ></textarea>
+      </form>
+    );
+  }
+};
