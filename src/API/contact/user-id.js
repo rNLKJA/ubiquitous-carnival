8fdc83903f5145290ae1eca2e795bee89151b46e
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-10-12 16:16:29 +1100

    Merge conflict resolve

--- src/API/contact/user-id.js
+++ src/API/contact/user-id.js
@@ -1,50 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contact.css";
 import fetchClient from "../axiosClient/axiosClient";
 // import qr_code from "./qr-code.png";
 // import hand_write from "./notes.png";
 
 const AddUser = () => {
-    const [contact, setContact] = useState( {userName: ""} );
-    const [userName, setUserName] = useState("");
-    const BASE_URL = "https://crm4399.herokuapp.com";
-    const submitUserID = async (e) => {
-        e.preventDefault();
+  const [contact, setContact] = useState({ userName: "" });
+  const [userName, setUserName] = useState("");
+  const BASE_URL = "https://crm4399.herokuapp.com";
+  const submitUserID = async (e) => {
+    e.preventDefault();
 
-        const contact = {
-            userName,
-        };
-
-        await fetchClient
-            .post(BASE_URL + "/contact/createContactByUserName", contact)
-            .then(res => console.log(res)) // TODO: duplicate account issue
-            .catch((err) => {
-                console.error(err);
-            });
-
-        setUserName("");
+    const contact = {
+      userName,
     };
 
-    return (
-        <React.Fragment>
-            <div className="sub-container">
-                <h1>Add by User ID</h1>
-                <form className="newID" method="POST" onSubmit={submitUserID}>
-                    <input
-                        name="firstName"
-                        type="text"
-                        placeholder="Please enter the User Name"
-                        onChange={(e) => setUserName(e.target.value)}
-                        value={userName}
-                        required
-                    />
-                    <input type="submit" value="Create"/>
-                    <br />
-                </form>
+    await fetchClient
+      .post(BASE_URL + "/contact/createContactByUserName", contact)
+      .then((res) => console.log(res)) // TODO: duplicate account issue
+      .catch((err) => {
+        console.error(err);
+      });
 
-            </div>
-        </React.Fragment>
-    );
+    setUserName("");
+  };
+
+  return (
+    <React.Fragment>
+      <div className="sub-container">
+        <h1>Add by User ID</h1>
+        <form className="newID" method="POST" onSubmit={submitUserID}>
+          <input
+            name="firstName"
+            type="text"
+            placeholder="Please enter the User Name"
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
+            required
+          />
+          <input type="submit" value="Create" />
+          <br />
+        </form>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default AddUser;
