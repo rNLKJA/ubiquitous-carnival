96e1e4089e6ec2d81b37c56628f8e6495c5d1db4
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-10-14 23:38:34 +1100

    Removed unused useEffect

--- src/API/person/qr.js
+++ src/API/person/qr.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useShowProfile } from "../../BackEndAPI/profileAPI";
 import { Link } from "react-router-dom";
 import QRCode from "qrcode";
