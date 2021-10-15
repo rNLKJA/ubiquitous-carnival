f5695c07302c7ef9824bad0d0b46cde1409ff9f0
Harrison Huang <hohuang@student.unimelb.edu.au>
2021-10-15 18:35:45 +1100

    improve Integration test

--- __tests__/integration_test/createRecordTest.js
+++ __tests__/integration_test/createRecordTest.js
@@ -51,7 +51,28 @@ describe("Integration test: Test for create Record", () => {
       });
   });
 
-  test("Test 2: Add a record without the meeting dateTime", () => {
+  test("Test 2: Add a record without contact_id", () => {
+    return agent
+      .post("/record/createRecord")
+      .set("Content-Type", "application/json")
+      .set("Authorization", jwtToken)
+      .send({
+        contact_id: null,
+        location: "University of Melbourne",
+        dateTime: "2021-10-01T10:28:10.018Z",
+        geoCoords: {
+            "lat": "122334545",
+            "lng": "52123456"
+        },
+        notes: "account"
+    })
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.text).toContain("Database query failed");
+      });
+  });
+
+  test("Test 3: Add a record without the meeting dateTime", () => {
     return agent
       .post("/record/createRecord")
       .set("Content-Type", "application/json")
@@ -65,14 +86,19 @@ describe("Integration test: Test for create Record", () => {
             "lng": "52123456"
         },
         notes: "account"
-    })
+      })
       .then((res) => {
         expect(res.statusCode).toBe(200);
+        expect(res.body.meetingPerson).toBe("6131e5b0e0accb25d09663f6");
+        expect(res.body.location).toBe("University of Melbourne");
         expect(res.body.dateTime).not.toBe(null);
+        expect(res.body.lat).toBe(122334545);
+        expect(res.body.lng).toBe(52123456);
+        expect(res.body.notes).toBe("account");
       });
   });
 
-  test("Test 3: Add a record with the meeting dateTime", () => {
+  test("Test 4: Add a record with the meeting dateTime", () => {
     return agent
       .post("/record/createRecord")
       .set("Content-Type", "application/json")
@@ -86,13 +112,20 @@ describe("Integration test: Test for create Record", () => {
             "lng": "52123456"
         },
         notes: "account"
-    })
+      })
       .then((res) => {
         expect(res.statusCode).toBe(200);
+        expect(res.body.meetingPerson).toBe("6131e5b0e0accb25d09663f6");
+        expect(res.body.location).toBe("University of Melbourne");
         expect(res.body.dateTime).toBe("2021-10-01T10:28:10.018Z");
+        expect(res.body.lat).toBe(122334545);
+        expect(res.body.lng).toBe(52123456);
+        expect(res.body.notes).toBe("account");
       });
   });
-/*
+  
+
+  /*
   test("Test 4: Add a record without the linkedAccount", () => {
     return agent
       .post("/record/createRecord")
