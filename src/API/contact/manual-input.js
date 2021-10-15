f4851559127c3ea03d59ef2acfe3e5f3113895bc
chuangyu-hscy <chuangyu-hscy@users.noreply.github.com>
2021-10-15 18:04:07 +1100

    Multi-field input in manual input page

--- src/API/contact/manual-input.js
+++ src/API/contact/manual-input.js
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom";
 import Heading from "../heading/heading.jsx";
 import NavBar from "../nav/Navbar";
 
+
 const AddUser = () => {
   useEffect(() => {
     document.title = "Add a new Contact";
@@ -18,8 +19,8 @@ const AddUser = () => {
 
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
+  const [emails, setEmails] = useState([]);
+  const [phones, setPhones] = useState([]);
   const [occupation, setOccupation] = useState("");
   // const [meetRecord, setMeetRecord] = useState("");
   const [note, setNote] = useState("");
@@ -27,11 +28,14 @@ const AddUser = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+		var email = ConvertListObjectToListValues(emails, "email");
+    var phone = ConvertListObjectToListValues(phones, "phone");
+
     const contact = {
       firstName,
       lastName,
-      email: [email],
-      phone: [phone],
+      email,
+      phone,
       occupation,
       portraits: "",
       // meetRecord,
@@ -48,8 +52,8 @@ const AddUser = () => {
     alert("You've create a new contact!");
     setFirstName("");
     setLastName("");
-    setEmail("");
-    setPhone("");
+    setEmails([]);
+    setPhones([]);
     setOccupation("");
     // setMeetRecord("");
     setNote("");
@@ -73,6 +77,63 @@ const AddUser = () => {
   //   console.log("posted");
   // };
 
+	const handleAddPhone = (e) => {
+    e.preventDefault();
+    setPhones([...phones, { phone: "" }]);
+  };
+
+	const handleAddEmail = (e) => {
+    e.preventDefault();
+    setEmails([...emails, { email: "" }]);
+  };
+
+	const removeHandler = (e, index, type) => {
+		e.preventDefault();
+		if (type === "phone") {
+			setPhones((prev) => prev.filter((item) => item !== prev[index]));
+		}
+
+		if (type === "email") {
+			setEmails((prev) => prev.filter((item) => item !== prev[index]));
+		}
+	};
+
+	const emailOnChange = (index, event) => {
+    event.preventDefault();
+    event.persist();
+
+    setEmails((prev) => {
+      return prev.map((item, i) => {
+        if (i !== index) {
+          return item;
+        }
+
+        return {
+          ...item,
+          [event.target.name]: event.target.value,
+        };
+      });
+    });
+  };
+
+	const phoneOnChange = (index, event) => {
+    event.preventDefault();
+    event.persist();
+
+    setPhones((prev) => {
+      return prev.map((item, i) => {
+        if (i !== index) {
+          return item;
+        }
+
+        return {
+          ...item,
+          [event.target.name]: event.target.value,
+        };
+      });
+    });
+  };
+
   return (
     <React.Fragment>
       <Heading />
@@ -89,10 +150,11 @@ const AddUser = () => {
           <button onClick={uploadImage}>Upload</button>
         </div> */}
 
-        <form className="contact-form" method="POST" onSubmit={handleSubmit}>
+        <form className="contact-form" method="POST" onSubmit={handleSubmit} style={{height: "98%", overflow: "scroll"}}>
           <label htmlFor="firstName">First Name: </label>
           <input
             name="firstName"
+						className='form-control'
             type="text"
             placeholder="Please enter the Last Name"
             onChange={(e) => setFirstName(e.target.value)}
@@ -103,6 +165,7 @@ const AddUser = () => {
           <label htmlFor="lastName">Last Name: </label>
           <input
             name="lastName"
+						className='form-control'
             type="text"
             placeholder="Please enter the First Name"
             onChange={(e) => setLastName(e.target.value)}
@@ -111,30 +174,77 @@ const AddUser = () => {
           ></input>
 
           <label htmlFor="email">E-mail: </label>
-          <input
-            name="email"
-            type="email"
-            placeholder="Please enter the E-mail"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            required
-          ></input>
+					{emails.map((mail, i) => {
+						return (
+							<div className="multi-field" >
+								<div className="multi-field-input" style={{display: "flex", flexDirection:"row", gap: "10px", width: "100%" }}>
+									<input
+										value={mail.email}
+                    type="email"
+                    name="email"
+                    className="form-control"
+                    required
+                    onChange={(e) => emailOnChange(i, e)}
+									></input>
+									<button
+												className="btn btn-info"
+												style={{
+													width: "40px",
+													height: "40px",
+													margin: "0px"
+												}}
+												onClick={(e) => removeHandler(e, i, "email")}
+											>
+												x
+									</button>
+								</div>
+							</div>
+						)
+					})}
+
+					<button className="btn btn-primary mt-2" onClick={handleAddEmail}>
+              Add Email
+          </button>
 
           <label htmlFor="phone">Phone: </label>
-          <input
-            name="phone"
-            type="tel"
-            pattern="[0-9]{10}"
-            maxLength={10}
-            placeholder="Please enter the phone number follow the pattern #### ### ###"
-            onChange={(e) => setPhone(e.target.value)}
-            value={phone}
-            required
-          ></input>
+          {phones.map((phone, i) => {
+						return (
+							<div className="multi-field" >
+								<div className="multi-field-input" style={{display: "flex", flexDirection:"row", gap: "10px", width: "100%" }}>
+									<input
+                    type="text"
+                    value={phone.phone}
+                    className="form-control"
+                    name="phone"
+                    required
+                    minLength={10}
+                    maxLength={10}
+                    onChange={(e) => phoneOnChange(i, e)}
+                  />
+									<button
+												className="btn btn-info"
+												style={{
+													width: "40px",
+													height: "40px",
+													margin: "0px"
+												}}
+												onClick={(e) => removeHandler(e, i, "phone")}
+											>
+												x
+									</button>
+								</div>
+							</div>
+						)
+					})}
+
+					<button className="btn btn-primary mt-2" onClick={handleAddPhone}>
+              Add Phone
+            </button>
 
           <label htmlFor="occupation">Occupation: </label>
           <input
             name="occupation"
+						className='form-control'
             type="text"
             placeholder="Please enter the occupation"
             onChange={(e) => setOccupation(e.target.value)}
@@ -153,6 +263,7 @@ const AddUser = () => {
 
           <label htmlFor="note">Notes: </label>
           <textarea
+						style={{minWidth: "100%"}}	
             name="note"
             type="text"
             placeholder="Add Notes"
@@ -174,3 +285,20 @@ const AddUser = () => {
 };
 
 export default AddUser;
+
+const ConvertListObjectToListValues = (items, type) => {
+  var result = [];
+  if (type === "phone") {
+    for (let i = 0; i < items.length; i++) {
+      result.push(items[i].phone);
+    }
+  }
+
+  if (type === "email") {
+    for (let i = 0; i < items.length; i++) {
+      result.push(items[i].email);
+    }
+  }
+
+  return result;
+};
