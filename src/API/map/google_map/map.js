3268b7ca0a65051d247130bb28375179b7d08379
Red_Peony <55085953+EcZww@users.noreply.github.com>
2021-10-21 23:12:51 +0800

    update CSS on map

--- src/API/map/google_map/map.js
+++ src/API/map/google_map/map.js
@@ -60,7 +60,7 @@ const Map = React.memo(() => {
   return (
     // Important! Always set the container height explicitly
     <div className="map-container">
-      <div style={{ height: "80vh", width: "100%" }} className="google-map">
+      <div style={{ height: "75vh", width: "100%" }} className="google-map">
         {/* google map api connection setup */}
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyBGlgQ8EI9WBIpx9HOraQyJAD7fFLbrWvE" }}
